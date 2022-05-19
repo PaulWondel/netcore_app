ce218987216c4903436caa1feec1af73184a6c28
Paul Wondel <p.r.wondel@gmail.com>
2022-05-19 23:38:27 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int number = 123;
+            double dnumber = 12.5D;
+            char cname = 'a';
+            bool boolean = true;
+            Console.WriteLine($"This line is pretty much print/println in other languages");
+            Console.WriteLine($"{number + number}, {dnumber + number}, {dnumber}, {name}, {cname}, {name + cname}, {boolean}");
+        }
+    }
+}
